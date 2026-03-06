adee25e2867fb699dc7f0146d992183c8450e758
Gideão dos Santos <gideao.d.santos@aluno.senai.br>
2026-03-06 08:18:33 -0300

    Sistema de Upload e organização de arquivos/Funcionando

---
+++ php/check_file.php
@@ -0,0 +1,30 @@
+<?php
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $tipo_upload = isset($_POST['tipo_upload']) ? $_POST['tipo_upload'] : 'normal';
+    $filename = isset($_POST['filename']) ? $_POST['filename'] : '';
+
+    if (empty($filename)) {
+        echo json_encode(['existe' => false, 'erro' => 'Nome do arquivo não fornecido']);
+        exit;
+    }
+
+    $diretorio_base = '../documentos/';
+    $pasta_destino = ($tipo_upload === 'kit') ? $diretorio_base . 'upload_kit/' : $diretorio_base . 'upload_normal/';
+
+    // Padroniza o nome do arquivo da mesma forma que o upload
+    $nomeSanitizado = preg_replace('/[^a-zA-Z0-9_.-]/', '_', $filename);
+    $caminhoCompleto = $pasta_destino . $nomeSanitizado;
+
+    if (file_exists($caminhoCompleto)) {
+        echo json_encode(['existe' => true]);
+    }
+    else {
+        echo json_encode(['existe' => false]);
+    }
+}
+else {
+    echo json_encode(['existe' => false, 'erro' => 'Método inválido']);
+}
+?>
